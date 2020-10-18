4822c6347afa0bc7c9e08e4e752f1cc2855b8063
NathSeller <nathseller99@outlook.com>
2020-10-18 02:26:10 +0100

    Updates

---
+++ precache-manifest.d589b5982ed8caff6fac29f8bda20651.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44d7f5b7de69877594895a92a5e1d3c8",
+    "url": "/covid/index.html"
+  },
+  {
+    "revision": "29740887f9b6393a5058",
+    "url": "/covid/static/css/main.5f5e5596.chunk.css"
+  },
+  {
+    "revision": "ff80ca4edeac89e9721a",
+    "url": "/covid/static/js/2.4cee9696.chunk.js"
+  },
+  {
+    "revision": "ac4ffa870128832fb53ad8bb4b491409",
+    "url": "/covid/static/js/2.4cee9696.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29740887f9b6393a5058",
+    "url": "/covid/static/js/main.d4dae8b0.chunk.js"
+  },
+  {
+    "revision": "062044bcf29db4febcc2",
+    "url": "/covid/static/js/runtime-main.45dcbfad.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid/static/media/image.d7265326.png"
+  }
+]);
